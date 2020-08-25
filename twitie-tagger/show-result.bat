cd22a8f679c0481504bf63fef828b8a785e919ec
Manex Agirrezabal <manex.agirrezabal@gmail.com>
2020-08-25 15:27:04 +0200

    Removed external repositories for Twitter data. Links are in Readme and paper.

--- twitie-tagger/show-result.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-java -jar twitie_tag.jar models\show-result.model corpora\ritter_eval.nolabels > t_eval.vcb-labelled
-python eval.py corpora\ritter_eval.stanford t_eval.vcb-labelled
-
--- twitie-tagger/show-result.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-java -jar twitie_tag.jar models/show-result.model corpora/ritter_eval.nolabels > t_eval.vcb-labelled
-python eval.py corpora/ritter_eval.stanford t_eval.vcb-labelled
-
